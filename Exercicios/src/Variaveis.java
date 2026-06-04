e0d31d94b02191aa15501c3de73460f84ba75843
garcia76-dev <aleprog815@gmail.com>
2026-06-04 10:53:06 -0300

    first commit

--- Exercicios/src/Variaveis.java
+++ Exercicios/src/Variaveis.java
@@ -7,16 +7,20 @@ public class Variaveis {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Seja bem-vindo! Digite seu nome completo:");
         String nome = scanner.nextLine();
+        System.out.println("nome"+nome);
 
         System.out.println("Agora digite o seu cpf:");
         double cpf = scanner.nextDouble();
 
+
         System.out.println("Digite sua idade:");
         int idade = scanner.nextInt();
+        System.out.println("idade"+idade);
 
         System.out.println("Digite sua altura:");
 
         double altura = scanner.nextDouble();
+        System.out.println("altura"+altura);
         scanner.nextLine();
 
         System.out.println("Digite seu estado civil:");
